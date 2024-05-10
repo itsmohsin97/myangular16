86ba8a851d8eedda47ed10c5aea6515c7ac6c5f1
Sultan <mohsinsultan14k@gmail.com>
2024-05-10 22:28:14 +0530

    Simple Chips using angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatRippleModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon'; 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatChipsModule,
+    ReactiveFormsModule,
+    MatRippleModule,
+    MatRippleModule,
+    MatAutocompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
